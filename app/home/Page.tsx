b809c7d7d0a19b7230078bd8cc3ed7d5b8c798f6
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2024-02-06 23:12:19 +0530

    my first Commit

---
+++ app/home/Page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+
+            <div className={"flex"}>
+                <div className={"flex bg-blue-900 w-2/12 h-screen"}>
+
+                </div>
+            </div>
+    );
+};
